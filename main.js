8a20df5c9321022462d9a23b9f162bfbb3aff35d
Naama Iluz <naamarin@g.jct.ac.il>
2024-10-09 11:18:46 +0300

    Adding option buttons to sidebar

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+function displayNotebooks() {
+    renderProducts(getProducts(notebooks));
+}
+
+function displayCraft() {
+    renderProducts(getProducts(craft));
+}
 function main() {
     renderProducts(getProducts(notebooks));
-    renderProducts(getProducts(craft));
 }
 
 main();
